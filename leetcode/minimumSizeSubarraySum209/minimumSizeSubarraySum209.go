acc08be5c25490007de8ca6cc33e4877d84732c1
lin <1125043965@qq.com>
2023-11-03 00:42:58 +0800

    11.03

---
+++ leetcode/minimumSizeSubarraySum209/minimumSizeSubarraySum209.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+//给定一个含有 n 个正整数的数组和一个正整数 target 。
+//找出该数组中满足其总和大于等于 target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+//示例 1：
+//输入：target = 7, nums = [2,3,1,2,4,3]
+//输出：2
+//解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+//示例 2：
+//输入：target = 4, nums = [1,4,4]
+//输出：1
+//示例 3：
+//输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+//输出：0
+
+func main() {
+
+	nums := make([]int, 0)
+	nums = append(nums, 2, 3, 1, 2, 4, 3)
+
+	res := minSubArrayLen2(7, nums)
+	fmt.Println(res)
+
+}
+
+func minSubArrayLen2(target int, nums []int) int {
+	i := 0
+	l := len(nums)  // 数组长度
+	sum := 0        // 子数组之和
+	result := l + 1 // 初始化返回长度为l+1，目的是为了判断“不存在符合条件的子数组，返回0”的情况
+	for j := 0; j < l; j++ {
+		sum += nums[j]
+		for sum >= target {
+			subLength := j - i + 1
+			if subLength < result {
+				result = subLength
+			}
+			sum -= nums[i]
+			i++
+		}
+	}
+	if result == l+1 {
+		return 0
+	} else {
+		return result
+	}
+}
